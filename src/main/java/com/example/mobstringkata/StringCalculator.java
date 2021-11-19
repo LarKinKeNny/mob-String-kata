e118f65f2bfc8d2994421cd8c8cb3dc9d18c4cba
Leo Smits <Leo.Smits@Magnabc.co.za>
2021-11-19 08:11:39 +0200

    reset

---
+++ src/main/java/com/example/mobstringkata/StringCalculator.java
@@ -0,0 +1,8 @@
+package com.example.mobstringkata;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StringCalculator {
+
+}
--- src/test/java/com/example/mobstringkata/MobStringKataApplicationTests.java
+++ src/test/java/com/example/mobstringkata/MobStringKataApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.mobstringkata;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,21 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MobStringKataApplicationTests {
 
-    @Test
-    void testEmptyString() {
-        int size = 3;
-        var cell = new Coordinate(0, 0);
-
-        boolean[][] matrix = new boolean[size][size];
-
-        matrix[0][1] = true;
-        matrix[1][0] = true;
-
-        int result = application.countNeighbours(matrix, size, cell);
-
-        assert(result == 2);
-    }
-
     @Autowired
-    private MainApplication application;
+    private StringCalculator application;
+
+    @Test
+    void emptyStringTest(){
+
+    }
 }
